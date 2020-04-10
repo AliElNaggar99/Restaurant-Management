bd67df394c25690d69ac95b1a232328330c3fde6
AliElNaggar99 <54042018+AliElNaggar99@users.noreply.github.com>
2020-04-10 22:59:34 +0200

    Updated \\ in GUI

--- Restaurant/GUI/GUI.h
+++ Restaurant/GUI/GUI.h
@@ -7,9 +7,9 @@
 #include "..\Rest\Order.h"
 
 #include "..\Rest\Cook.h"
-#include"../VeganCook.h"
-#include"../NormalCook.h"
-#include"../VipCook.h"
+#include"..\Rest\VeganCook.h"
+#include"..\Rest\NormalCook.h"
+#include"..\Rest\VipCook.h"
 
 #include "..\Generic_DS\Queue.h"
 
